00d4b952849f2670eea984ec8d41ac75a4acde56
Andrey Gavriluk <bugabuga.008@gmail.com>
2021-01-15 09:53:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.eeb074d27a27736d6ed59274a8539a39.js"
 );
 
 self.addEventListener('message', (event) => {
